1ec9daa23f7e169515cba5812c2e1825c981e19e
unknown <al_bcs96@hotmail.com>
2023-07-20 19:39:06 -0500

    semana08-dia01-paises y ciudades

--- semana08/dia01-javascript-es6/modules/main.js
+++ semana08/dia01-javascript-es6/modules/main.js
@@ -1,12 +1,38 @@
-//  import { regions } from "./constants";
-
+// import constants from './constants.js'
 import { fetchCountries } from './services.js'
 import { renderCountries } from './utils.js'
 
-document.addEventListener('DOMContentLoaded', function () {
-  console.log('EL DOM se cargo totalmente')
+const searchInput = document.querySelector('.app__search')
 
-  fetchCountries()
-    .then(response => renderCountries(response))
+let countryData = []
+
+searchInput.addEventListener('input', (event) => {
+  const value = event.target.value
+
+  const filteredCountries = countryData.filter(country => {
+    const loweredName = country.name.common.toLowerCase()
+    const joinedCapital = country.capital.join(',') // Une todos los elementos de un arreglo en una cadena de texto
+    // console.log(joinedCapital)
+    const loweredCapital = joinedCapital.toLowerCase()
+    const loweredValue = value.toLowerCase()
+
+    // DONE: Adicionalmente necesitamos filtrar por capital
+
+    return loweredName.includes(loweredValue) || loweredCapital.includes(loweredValue)
+  })
+
+  renderCountries(filteredCountries)
 })
 
+document.addEventListener('DOMContentLoaded', async () => {
+  // console.log('EL DOM se cargo totalmente')
+
+  // fetchCountries()
+  //   .then(response => renderCountries(response))
+
+  const data = await fetchCountries()
+
+  countryData = data
+
+  renderCountries(data)
+})
